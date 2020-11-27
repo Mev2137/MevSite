46f8cf81ea796866ce63e075f832ae7ff588e23e
Mev2137 <szymongrysiewicz@gmail.com>
2020-11-27 23:40:55 +0100

    booble

---
+++ booble/script.js
@@ -0,0 +1,9 @@
+function mason() {
+  let link =
+    "https://www.google.com/search?q=" + document.getElementById("mason").value;
+  ///if (document.getElementById("mason").value > 1) {
+  window.open(link);
+  // } else {
+  // window.open("https://nhentai.net/random/");
+  //}
+}
