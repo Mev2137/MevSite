de878a7d68d21210390360c988a2ae261ed3d2d2
Mev2137 <szymongrysiewicz@gmail.com>
2020-12-05 01:23:44 +0100

    jd

--- js/script.js
+++ js/script.js
@@ -1,50 +1,49 @@
+var imageUrl = "url('images/giphy.gif')";
+var audio = new Audio("images/song.mp3");
+
 document.getElementById("searchForm").onsubmit = (e) => {
-	e.preventDefault();
-	const linkEl = document.getElementById("query");
-	let link = "https://www.google.com/search?q=";
-	link += encodeURIComponent(linkEl.value);
-	linkEl.value = "";
-	window.open(link);
+  e.preventDefault();
+  linkEl = document.getElementById("query");
+  link = "https://www.google.com/search?q=";
+  link += encodeURIComponent(linkEl.value);
+
+  /* if (linkEl.value == "ryuko")
+	{
+		document.body.style.backgroundImage=imageUrl;
+		
+		document.getElementsByTagName("title")[0].innerHTML = "RYUKO"
+		audio.play();
+	} */
+  window.open(link);
+  linkEl.value = "";
 };
+const DarlingCode = ["r", "y", "u", "k", "o"];
+let darlingCodePosition = 0;
 
-if (document.addEventListener) {
-	document.addEventListener("DOMContentLoaded", function () {
-		loaded();
-	});
-} else if (document.attachEvent) {
-	document.attachEvent("onreadystatechange", function () {
-		loaded();
-	});
-}
+const darlingCodeChecker = (event) => {
+  const inputs = ["input", "select", "button", "textarea"];
+  const aEl = document.activeElement;
+  if (aEl && inputs.indexOf(aEl.tagName.toLowerCase()) !== -1) return;
+  const key = event.key.toLowerCase();
+  const requiredKey = DarlingCode[darlingCodePosition].toLowerCase();
+  if (key != requiredKey) {
+    darlingCodePosition = 0;
+    return;
+  }
+  darlingCodePosition++;
+  if (darlingCodePosition != DarlingCode.length) return;
+  darlingCodePosition = 0;
+  document.body.style.backgroundImage = imageUrl;
 
-function loaded() {
-	setInterval(loop, 800);
-}
+  audio.play();
+  setInterval(loop, 800);
+};
 
 let x = 0;
 
-const TitleText = [
-	"B",
-	"BO",
-	"BOO",
-	"BOOB",
-	"BOOBl",
-	"BOOBle",
-	"BOOBle -",
-	"BOOBle - M",
-	"BOOBle - Me",
-	"BOOBle - Mev",
-	"BOOBle - MevB",
-	"BOOBle - MevBr",
-	"BOOBle - MevBro",
-	"BOOBle - MevBrow",
-	"BOOBle - MevBrows",
-	"BOOBle - MevBrowse",
-	"BOOBle - MevBrowser",
-	"uwu",
-];
+const TitleText = ["R", "RY", "RYU", "RYUK", "RYUKO", "uwu"];
 
 function loop() {
-	document.getElementsByTagName("title")[0].innerHTML =
-		TitleText[x++ % TitleText.length];
+  document.getElementsByTagName("title")[0].innerHTML =
+    TitleText[x++ % TitleText.length];
 }
